a89ddc02a324e89fcb635f5b7de78ec36928346b
diovanne85 <dpsagosoy85@gmail.com>
2023-11-08 02:35:05 +0800

    add pages

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <ul className="nav">
+        <li>
+          <a href="/aboutMe">About Me</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
